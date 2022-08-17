47ab1a69f6b7f1f6d0369ae49431a1a0d5379fad
lilys tacos <lilyannek@gmail.com>
2022-08-16 17:53:36 -0700

    index.js file within routes folder

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,9 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send("You've used the wrong route");
+});
+
+module.exports = router;
