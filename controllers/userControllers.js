15387e45a1739afe2161a07184e4a415d9609e17
lilys tacos <lilyannek@gmail.com>
2022-08-16 18:17:24 -0700

    refactored server.js and added connection.js file

---
+++ controllers/userControllers.js
@@ -0,0 +1,9 @@
+const { User } = require("../models");
+
+module.exports = {
+  getAllUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const db = require("./config/connection.js");
+const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-app.use(require("./routes"));
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network",
-  {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-app.listen(PORT, () => console.log(`Connected to localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
