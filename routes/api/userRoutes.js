86d2d951f650a0a6f1edf8255fd200cbe6695e22
lilys tacos <lilyannek@gmail.com>
2022-08-16 17:57:05 -0700

    user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,7 +10,13 @@ const {
   deleteFriend,
 } = require("../../controllers/userControllers.js");
 
-// GET all users and POST users /api/users
+// GET all users and POST users
 router.route("/").get(getAllUsers).post(createUser);
 
-router.route("/:id").get;
+// GET, PUT, and DELETE one user by id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// POST and DELETE a friend
+router.route("/:id/friends/:friendsId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
