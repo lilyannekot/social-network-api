3c00f5fa95f84e782e22de8fec49894571c5ff94
lilys tacos <lilyannek@gmail.com>
2022-08-17 17:57:14 -0700

    reaction schema, get total reactions, create Thought model, export

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,7 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema, Types, trusted } = require("mongoose");
+const moment = require("moment");
 
+// Thought Schema
 const thoughtSchema = new Schema(
   {
     username: {
@@ -11,8 +13,8 @@ const thoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      minLength: 1,
-      maxLength: 280,
+      minlength: 1,
+      maxlength: 280,
     },
   },
   {
@@ -31,3 +33,51 @@ const thoughtSchema = new Schema(
     id: false,
   }
 );
+
+// Reaction Schema
+const reactionSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+  },
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+  },
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: trusted,
+    },
+    id: false,
+  }
+);
+
+// Total count of reactions
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.lengths;
+});
+
+// Create Thought model through thoughtSchema
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
