f7a60e424fddb3b22fce0b1fe40b5aa658b75359
lilys tacos <lilyannek@gmail.com>
2022-08-17 18:12:26 -0700

    create a new user and update an existing user by id in user controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -19,4 +19,29 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "No user could be found with that id " })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
