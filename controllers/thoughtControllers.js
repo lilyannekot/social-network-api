7c246c1fb6b93bdbc09fe1523c7f189e36653177
lilys tacos <lilyannek@gmail.com>
2022-08-18 19:30:43 -0700

    get single thought by id

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,13 +1,23 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-// Get all thoughts
+  // Get all thoughts
   getAllThoughts(req, res) {
     Thought.find()
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
   // Get single thought by id
-  
-
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.id })
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: "No thought could be found with that id" })
+          : res.json(thoughts)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //
 };
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 
 const {
-    getAllThoughts,
-    getThoughtById,
-    createThought,
-    updateThought,
-    deleteThought,
-    createReaction,
-    deleteReaction,
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtControllers");
 
 // Get all thoughts
 router.route("/").get(getAllThoughts);
+
+router.route("/:id").get(getThoughtById);
