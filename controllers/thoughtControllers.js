c028f7cbbabf74943d24e24a3969c018725e9799
lilys tacos <lilyannek@gmail.com>
2022-08-18 19:39:50 -0700

    create a new thought controller

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -19,5 +19,28 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  //
+  // Create a new thought
+  createThought(req, res) {
+    Thought.create(req.body).then((thoughts) => {
+      return User.findOneAndUpdate(
+        { username: req.body.id },
+        { $push: { thoughts: thoughts._id } },
+        { new: true }
+      )
+        .then((user) => {
+          if (user) {
+            res.json({
+              message: "Your thought has been created successfully!",
+            });
+          } else {
+            res
+              .status(404)
+              .json({
+                message: "There was an error when creating your thought",
+              });
+          }
+        })
+        .catch((err) => res.status(500).json(err));
+    });
+  },
 };
